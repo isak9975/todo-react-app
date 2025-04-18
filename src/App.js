62f3514d473b37636d704cfc78226478a97e5147
isak9975 <isak9975@gmail.com>
2025-04-18 17:52:51 +0900

    42일차

--- src/App.js
+++ src/App.js
@@ -5,42 +5,50 @@ import Todo from './Todo';
 import Example from './Example.js';
 import { Farewell } from './Greetings.js';
 import { useState } from 'react';
-import {List, Paper} from '@mui/material'
+import {Container,List, Paper} from '@mui/material'
+import AddTodo from './AddTodo.js';
+
+//Container
+//레이아웃의 가로폭을 제한하고, 중앙 정렬 및 기본 패딩을 자동으로 적용해주는 컴포넌트
+//maxWidth : 최대 너비를 지정(xs, sm, md, lg, xl, false)
+//fixed : maxWidth와 관계 없이 항상 고정폭 적용.
+
 
 function App() {
   //하나의 할 일을 객체로 관리할 것이다.
   //id, title, done => 하나로 묶어 객체로 관리
-  const [item,setItem] = useState([
+  const [items,setItems] = useState([])
 
-    {
-    id :"0",
-    title:"Hello world 1",
-    done:true
-  },{
-    id :"1",
-    title:"Hello world 2",
-    done:false
-  },{
-    id :"2",
-    title:"Hello world 3",
-    done:false
-  }
+  
+    //Todo를 추가하기 위한 백엔드 콜을 대신할 가짜 함수를 만들어보자.
+    const add = (item) => {
+        //newItem 객체가 하나의 Todo
+        const newItem = {
+            ...item,
+            id:"ID-"+item.length,
+            // done:false
+        }
 
-])
+        //상태를 변화시키는 함수를 호출하면 state의 변경사항이 화면에 적용이 된다.
+        //화살표 함수로 기존의내용(prev) 위에 추가하는거. => 기존내용 + newItem
+        setItems(prev=>[...prev,newItem])
+        console.log("items : ",[...items, newItem])
+    }
+
 
 //react는 key속성에 들어잇는 값을 참고해서, 리스트의 요소가 변경될 경우
 //어떤 요소가 변경되었는지 빠르게 파악할 수 있다.
 //  ㄴ key없어도 동작은 하네.
 //배열에 내용이 있을때만 렌더링 해라
-const todoItems = item.length > 0 && 
+const todoItems = items.length > 0 && 
   //Paper컴포넌트
   //종이 같은 표면 효과를 제공하는 컨테이너 컴포넌트
   //elevation(그림자깊이)를 통해 높낮이를 표현하고
   //배경색과 글미자 효과로 콘텐츠를 돋보이게 한다.
 <Paper style={{margin: 16}}>
   <List>{/* 일련의 항목을 세로로 나열하는 컨테이너 역활 */}
-    {item.map((items) => (
-      <Todo item={items} key={items.id} />
+    {items.map((item) => (
+      <Todo item={item} key={item.id} />
     ))}
   </List>
 </Paper>
@@ -49,27 +57,17 @@ const todoItems = item.length > 0 &&
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo1} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello React
-        </a> */}
-        {/* <Example/>
-        <Farewell name="김철수"/> */}
+    <div>
+      <Container maxWidth="md">
+    
         {/* Todo로 item객체를 전달. */}
-        {todoItems}
-          
+        <AddTodo add={add}/>   {/* AddTodo에 add함수를 전달 */}
         
-      </header>
+        {todoItems}
+        
+        
+      {/* </header> */}
+      </Container>
     </div>
   );
 }
